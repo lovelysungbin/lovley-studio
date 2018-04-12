9afe3faaac603004aff28a1adbe6ac0fbe792967
Unknown <crazy.ysb@gmail.com>
2018-04-12 22:02:31 +0900

    numberbaseball

--- code/numberbaseball.c
+++ code/numberbaseball.c
@@ -1,56 +1,47 @@
-#include <stdio.h>
+#include <stdio,h>
+#include <time.h>
 
 int main()
 {
-	int num1;
-	int num2;
-	int num3;
-	int in1;
-	int in2;
-	int in3;
-	int st = 0;
-	int ball = 0;
-	
-	printf("숫자를 입력하세요:");
-	scanf("%d %d %d",&num1,&num2,&num3);
-	
-	while(st< 3)
-	{
-		st = 0;
-		fflush(stdin);
-		printf("맞추세요!!!");
-		scanf("%d %d %d",&in1,&in2,&in3);	
-		if(num1==in1)
+	int i;
+	int k;
+	int num[3];
+	int in[3];
+	int turn = 1;
+	int ball=0;
+	int st=0;
+	int out=3;
+	srand((unsigned)time(NULL));
+	while(num[0]==num[1]||num[0]==num[3]||num[2]==num[3])
+	{	
+		for(i=0;i<3;i++)
 		{
-			st++;
+		num[i]=rand() % 10;
 		}
-		else if(in1==num2||in1==num3)
-		{
-			ball++;
-		}
-		if(num2==in2)
-		{
-			st++;
-		}
-		else if(in2==num1||in2==num3)
-		{
-			ball++;
-		}
-		if(num3==in3)
-		{
-			st++;
-		}
-		else if(in3==num1||in3==num2)
-		{	
-			ball++;
-		}
-		printf("strike:%d,ball:%d\n",st,ball);
-		ball = 0;
-		in1 = 0;
-		in2 = 0;
-		in3 = 0;
 	}
-	printf("GREAT!");
+	printf("시작!!!!!");
+	while()
+	{
+		scanf("%d %d %d",&in[1],&in[2],&in[3]);
+		for(i=0;i<3;i++)
+		{
+			for(k=0;k<3;k++)
+			{
+				if(in[i]==num[k])
+				{
+					if(in[i]==num[i])
+					{
+						st++;
+					}
+					else
+					{
+						ball++
+					}
+				}
+			}
+		}
+		printf("turn%d st:")
+	} 
 	
 	return 0;
 }
